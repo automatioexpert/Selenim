2d0de5048bbe24fea4972a5a310ecbf776be249b
Valmiki Vishwakarma <you@example.com>
2022-11-26 18:30:16 +0530

    code commit.

---
+++ SeleniumPractise/src/main/java/Nov26/T1.java
@@ -0,0 +1,19 @@
+package Nov26;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class T1 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://pldthome.com/");
+		driver.findElement(By.cssSelector(".header_login-btn")).click();
+		driver.close();
+
+	}
+
+}
